gradient fix

Task1/Regression.py
@@ -13,15 +13,25 @@ class LogisticRegression:
         self.train = np.hsplit(train_data, (0, self.column_number-1))[1]
         self.target = np.hsplit(train_data, (0, self.column_number-1))[2]
 
-    def Q(self, w):
-            s = 0
-            for i in range(self.row_number):
-                power = -1*self.target[i][0]*np.dot(w, self.train[i][:])
-                s = s + math.log((1+math.exp(power)), math.e)
-            return s
+    def gradQ(self, w):
+        grad = np.zeros(len(w))
+        for i in range(len(w)):
+            grad_i = 0
+            for j in range(self.train.shape[0]):
+                M = -1 * self.target[j][0] * np.dot(w, self.train[j])
+                grad_i += np.exp(M) / (1 + np.exp(M)) * -1 * self.target[j][0] * self.train[j][i]
+            grad[i] = grad_i
+        return grad
 
-    def find_weights(self, initial_weights):
-        return grad.gradient_descent(self.Q, initial_weights)
+    def Q(self, w):
+        s = 0
+        for i in range(self.row_number):
+            power = -1*self.target[i][0]*np.dot(w, self.train[i][:])
+            s = s + math.log((1+math.exp(power)), math.e)
+        return s
+
+    def find_weights(self, initial_weights, accuracy):
+        return grad.gradient_descent(self.Q, initial_weights, accuracy, self.gradQ)
 
 
 

Task1/gradientDescent.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 
-def gradient_descent(function, initial):
+def gradient_descent(function, initial, accuracy, gradQ):
     def find_grad():
         x = np.zeros(number)
         x[:] = initial
@@ -22,19 +22,22 @@ def gradient_descent(function, initial):
                 status2 = 0
         return step1
     eps = 0.1
-    delta = 0.5
-    h = 0.0001
+    delta = 0.1
+    h = 0.000000001
     step = 0.01
     number = initial.shape[0]
     grad = np.zeros(number)
     status = 1
+    counter = 0
     while status != 0:
-        find_grad()
+        counter += 1
+        grad = gradQ(initial)
         step = choose_step(step)
         initial = initial - grad * step
-        #print step
-        if np.all(grad*step <= 0.001) and np.all(grad*step >= -0.001):
+        print grad, step
+        if np.all(grad*step <= accuracy) and np.all(grad*step >= -accuracy):
             status = 0
     norma = map(lambda x: x*x, initial)
     norma = math.sqrt(reduce(lambda x, y: x+y, norma))
-    return initial/norma
+    print counter, grad
+    return initial/norma, counter

Task1/main.py
@@ -6,14 +6,14 @@ import math
 import support
 
 
-def main(Expected1, Expected2, Dispersion1, Dispersion2, Number1, Number2):
+def main(Expected1, Expected2, Dispersion1, Dispersion2, Number1, Number2, accuracy):
     fig = pb.figure()
     data, axes = support.gen_data1(Expected1, Expected2, Dispersion1, Dispersion2, Number1, Number2, fig)
     l_regression = reg.LogisticRegression()
     l_regression.fit(data)
 
     weights_by_grad = np.zeros(Expected1.shape[0]+1)
-    weights_by_grad = l_regression.find_weights(weights_by_grad)
+    weights_by_grad, N = l_regression.find_weights(weights_by_grad, accuracy)
 
     weights_by_scipy = np.zeros(Expected1.shape[0]+1)
     weights_by_scipy = minimize(l_regression.Q, weights_by_scipy, method='nelder-mead')
@@ -22,17 +22,18 @@ def main(Expected1, Expected2, Dispersion1, Dispersion2, Number1, Number2):
     norma2 = math.sqrt(reduce(lambda x, y: x+y, norma2))
     weights_by_scipy.x /= norma2
 
-    print weights_by_grad, weights_by_scipy.x
+    #print weights_by_grad, weights_by_scipy.x
 
     support.draw(weights_by_grad, 'black', axes)
     support.draw(weights_by_scipy.x, 'yellow', axes)
     pb.show()
+    return N
 
-Expected1 = np.array([1, 1])
-Expected2 = np.array([1, 1])
+Expected1 = np.array([1, 4])
+Expected2 = np.array([4, 3])
 Dispersion1 = 1
-Dispersion2 = 1
-Number1 = 50
-Number2 = 50
-
-#main(Expected1, Expected2, Dispersion1, Dispersion2, Number1, Number2)
+Dispersion2 = 1.5
+Number1 = 20
+Number2 = 20
+accuracy = 0.001
+print main(Expected1, Expected2, Dispersion1, Dispersion2, Number1, Number2, accuracy)